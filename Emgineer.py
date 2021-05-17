d2a52e8f1c77485527b1846204c18f0b66039fda
sahilsaha7773 <sahilsaha7773@gmail.com>
2021-05-17 16:24:12 +0530

    Added queue and mail features

--- Emgineer.py
+++ Emgineer.py
@@ -6,10 +6,16 @@ import youtube_dl
 import random
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReg, Request
+import smtplib
+from pymitter import EventEmitter
+
+ee = EventEmitter()
+
 load_dotenv()
 
 # Get the API token from the .env file.
 DISCORD_TOKEN = os.getenv("discord_token")
+EMAIL_PASS = os.getenv("email_pass")
 
 intents = discord.Intents().all()
 client = discord.Client(intents=intents)
@@ -34,6 +40,8 @@ ffmpeg_options = {
     'options': '-vn'
 }
 song_queue = []
+auth_queue = []
+currently_playing = '-1'
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 class YTDLSource(discord.PCMVolumeTransformer):
@@ -76,22 +84,93 @@ async def leave(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_connected():
         await voice_client.disconnect()
+        song_queue.clear()
     else:
         await ctx.send("The bot is not connected to a voice channel.")
 
+async def queue_play(ctx):
+    voice_channel = ctx.message.guild.voice_client
+    while len(song_queue)!=0:
+        await ctx.send(f'**Searching:** {song_queue[0]}')
+        filename = await YTDLSource.from_url(song_queue[0], loop=bot.loop)
+        os.rename(filename, filename.split(".")[0]+".mp3")
+        voice_channel.play(discord.FFmpegPCMAudio(filename.split(".")[0]+".mp3"))
+        await ctx.send('**Now playing:** {}'.format(filename.split(".")[0]))
+        song_queue.pop(0)
+
+@bot.command(name='add', help='Add to queue')
+async def add(ctx):
+    titles = ctx.message.content[7:].split(',')
+    for title in titles:
+        title = title.strip()
+        song_queue.append(title)
+        auth_queue.append(f'**{str(ctx.message.author)}**')
+        await ctx.send(f'- **{title}** added to queue\n**Queue position: **{len(song_queue)}\nRequested by **{ctx.message.author}**')
+
+@bot.command(name='queue', help='View Song queue')
+async def queue(ctx):
+    voice_channel = ctx.message.guild.voice_client
+    await ctx.send("**Here's your Queue:**")
+    queue = ''
+    if not voice_channel.is_playing():
+        i = 1
+        for s in song_queue:
+            queue+=f'\n> {i}. **{s}**, requested by {auth_queue[i-1]}'
+            i+=1
+    else:
+        queue = f'> 1. **Currently Playing**: {currently_playing}'
+        i = 2
+        for s in song_queue:
+            queue+=f'\n> {i}. **{s}**, requested by {auth_queue[i-2]}'
+            i+=1
+    await ctx.send(queue)
+
+@bot.command(name='fs', help='Skip the current song')
+async def fs(ctx):
+    global currently_playing
+    if len(song_queue)==0:
+        await ctx.send('**Your Queue is empty.**')
+        return
+    await ctx.send(f'Skipping {currently_playing}')
+    voice_channel = ctx.message.guild.voice_client
+    await ctx.send(f'**Searching:** {song_queue[0]}')
+    filename = await YTDLSource.from_url(song_queue[0], loop=bot.loop)
+    os.rename(filename, filename.split(".")[0]+".mp3")
+    voice_channel.stop()
+    voice_channel.play(discord.FFmpegPCMAudio(filename.split(".")[0]+".mp3"))
+    await ctx.send('**Now playing:** {}'.format(filename.split(".")[0]))
+    currently_playing = f'**{filename.split(".")[0]}**' + f', requested by {auth_queue[0]}'
+    print('jdas', currently_playing)
+    song_queue.pop(0)
+
 @bot.command(name='play', help='To play song')
 async def play(ctx):
+    global currently_playing
     server = ctx.message.guild
     voice_channel = ctx.message.guild.voice_client
     title = ctx.message.content[8:]
-    print(song_queue)
-    async with ctx.typing():
-        await ctx.send(f'**Searching:** {title}')
-        filename = await YTDLSource.from_url(title, loop=bot.loop)
+    voice_channel = ctx.message.guild.voice_client
+    if ctx.message.guild.voice_client.is_playing():
+        print("is_playing")
+        await ctx.send(f'> **Currently playing:** {currently_playing}\n> To add to queue use "add" command.\n> To view queue use "queue" command.')
+    else:
+        if len(song_queue)==0:
+            await ctx.send(f'**Your Queue is empty.\nAutomatically adding to Queue.**')
+            titles = ctx.message.content[8:].split(',')
+            for title in titles:
+                title = title.strip()
+                song_queue.append(title)
+                auth_queue.append(f'**{str(ctx.message.author)}**')
+                await ctx.send(f'- **{title}** added to queue\n**Queue position: **{len(song_queue)}\nRequested by **{ctx.message.author}**')
+        await ctx.send(f'**Searching:** {song_queue[0]}')
+        filename = await YTDLSource.from_url(song_queue[0], loop=bot.loop)
         os.rename(filename, filename.split(".")[0]+".mp3")
         voice_channel.play(discord.FFmpegPCMAudio(filename.split(".")[0]+".mp3"))
-        song_queue.append(title)
         await ctx.send('**Now playing:** {}'.format(filename.split(".")[0]))
+        currently_playing = f'**{filename.split(".")[0]}**' + f', requested by {auth_queue[0]}'
+        print('jdas', currently_playing)
+        song_queue.pop(0)
+        auth_queue.pop(0)
 
 
 @bot.command(name='pause', help='This command pauses the song')
@@ -157,7 +236,22 @@ async def xvideos(ctx):
     except:
         await ctx.send("No matching panu images")
 
+@bot.command(name='mail', help='to send email')
+async def mail(ctx):
+    try:
+        title = ctx.message.content[8:]
+        credentials = title.split(" ")
+        mail = credentials[0]
+        msg = title.replace(credentials[0],"")
 
+        server = smtplib.SMTP_SSL("smtp.gmail.com",465)
+        server.login("lastbenchdevs@gmail.com", EMAIL_PASS)
+        server.sendmail("lastbenchdevs@gmail.com",mail,msg)
+        server.quit()
+        await ctx.send("Email send succesfully to "+mail)
+    except:
+        await ctx.send("Messege Not Send")
+        
 if __name__ == "__main__" :
     bot.run(DISCORD_TOKEN)
     print("My Ready is Body")
